4dc0b826eaae0761385c251238d42174b3ab61e0
boyo <boyo@jitu.mshome>
2019-07-11 17:02:52 +0300

    automated deploy to dockerhub change repo

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('nkirui2030/docker-nodejs-demo')
+            repositoryName('nkirui2030/nodejsjenkins')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
